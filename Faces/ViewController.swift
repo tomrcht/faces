4f0688bb19b7a5514273bb4aa0caf216475c8568
tomrcht <tom.roch4t@gmail.com>
2021-10-30 19:30:06 +0200

    Initial Commit

---
+++ Faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Faces
+//
+//  Created by Tom Rochat on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
