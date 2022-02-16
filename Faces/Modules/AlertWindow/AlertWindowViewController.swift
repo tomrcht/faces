6eb26fc126665b0c397d006ca66c097936ec1d15
tomrcht <tom.roch4t@gmail.com>
2022-02-16 17:47:01 +0100

    wip - add button to hide window when opened

--- Faces/Modules/AlertWindow/AlertWindowViewController.swift
+++ Faces/Modules/AlertWindow/AlertWindowViewController.swift
@@ -15,7 +15,17 @@ final class AlertWindowViewController: UIViewController {
         button.titleLabel?.numberOfLines = 0
         button.titleLabel?.textAlignment = .center
         button.setTitleColor(.systemBlue, for: .normal)
-        button.addTarget(self, action: #selector(spawnAlertWindow), for: .touchUpInside)
+        button.addTarget(self, action: #selector(showAlertWindow), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let hideWindowButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Hide the alert window", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.lightGray, for: .disabled)
+        button.addTarget(self, action: #selector(hideAlertWindow), for: .touchUpInside)
+        button.isEnabled = false
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -35,20 +45,36 @@ final class AlertWindowViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(windowButton)
+        view.addSubview(hideWindowButton)
         NSLayoutConstraint.activate([
             windowButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             windowButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             windowButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             windowButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            hideWindowButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            hideWindowButton.topAnchor.constraint(equalTo: windowButton.bottomAnchor, constant: 24),
         ])
     }
 
     @objc
-    private func spawnAlertWindow() {
+    private func showAlertWindow() {
         alertWindow.isHidden = false
         UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut) {
             self.alertWindow.layer.opacity = 1
-        } completion: { _ in }
+        } completion: { _ in
+            self.hideWindowButton.isEnabled = true
+        }
+    }
+
+    @objc
+    private func hideAlertWindow() {
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut) {
+            self.alertWindow.layer.opacity = 0
+        } completion: { _ in
+            self.alertWindow.isHidden = true
+            self.hideWindowButton.isEnabled = false
+        }
     }
 }
 
